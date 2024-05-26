06bec58274e3d4091d2a64d969091d8350e19e10
petrz <zamecnikpetr@protonmail.com>
2024-05-27 00:14:47 +0200

    scenes

--- Scripts/Player_Controller.cs
+++ Scripts/Player_Controller.cs
@@ -1,8 +1,14 @@
 using Godot;
 using System;
 
-public partial class Player_Controller : Node
+public partial class Player_Controller : CharacterBody2D
 {
+	private float _speed = 100.0f;
+	private float _jumpSpeed = -400.0f;
+
+	public float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +17,15 @@ public partial class Player_Controller : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		var velocity = Velocity;
+		velocity.Y += 9.8f * (float)delta;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		var velocity = Velocity;
+		
+		velocity.Y += 9.8f * (float)delta;
+
 	}
 }
