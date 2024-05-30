18c10507ae6e864f4706ca65a4dea87094e9e40e
petrz <zamecnikpetr@protonmail.com>
2024-05-30 22:04:38 +0200

    changed approach, testing walls

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -7,7 +7,6 @@ public partial class GameController : Node
     private const int BackgroundLayer = -1;
     private const int WallLayer = 0;
     private const int CellSize = 16;
-    private const int StartingPlatformHeight = 3;
 
     private readonly List<Vector2I> _backgroundTileCoords = new List<Vector2I>
     {
@@ -35,24 +34,14 @@ public partial class GameController : Node
         new Vector2I(13, 3)
     };
 
-    private readonly Vector2I _leftWallAtlasCoords = new Vector2I(3, 6);
-    private readonly Vector2I _rightWallAtlasCoords = new Vector2I(1, 6);
-    
-    private readonly Vector2I _groundTileAtlasCoord = new Vector2I(2, 1);
-    private readonly Vector2I _groundTileAtlasCoord2 = new Vector2I(2, 5);
-    private readonly Vector2I _groundTileAtlasCoord3 = new Vector2I(2, 2);
-
 
     private TileMap _backgroundTileMap;
     private TileMap _wallTileMap;
+
     private RandomNumberGenerator _rng = new RandomNumberGenerator();
     private CharacterBody2D _player;
-
-    private int _leftBoundary = -20;
-    private int _rightBoundary = 20;
-    private int _topBoundary = 20;
-    private int _bottomBoundary = 0;
-
+    private StaticBody2D _leftWall;
+    private StaticBody2D _rightWall;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -60,39 +49,44 @@ public partial class GameController : Node
         _backgroundTileMap = GetNode<TileMap>("BackgroundTileMap");
         _wallTileMap = GetNode<TileMap>("WallTileMap");
         _player = GetNode<CharacterBody2D>("Player");
-
+        _leftWall = GetNode<StaticBody2D>("LeftWall");
+        _rightWall = GetNode<StaticBody2D>("RightWall");
+        
         _backgroundTileMap.ZIndex = BackgroundLayer;
         _wallTileMap.ZIndex = WallLayer;
-        
-
-        GenerateStartingPlatform();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
-        var playerPosition = _player.GlobalPosition;
+        var playerGlobalPosition = _player.GlobalPosition;
+
+        if (Input.IsActionJustPressed("debug_key"))
+        {
+            GD.Print("Player Global Position:", playerGlobalPosition);
+            GD.Print("Player X position tiles:", CalculateTilesFromPixels((int)playerGlobalPosition.X));
+            GD.Print("Player Y position tiles:", CalculateTilesFromPixels((int)playerGlobalPosition.Y));
+        }
+        
+        UpdateWallPosition(playerGlobalPosition);
+    }
+
+    private void UpdateWallPosition(Vector2 playerGlobalPosition)
+    {
+        _leftWall.GlobalPosition = _leftWall.GlobalPosition with { Y = playerGlobalPosition.Y };
+        _rightWall.GlobalPosition = _rightWall.GlobalPosition with { Y = playerGlobalPosition.Y };
     }
 
 
-    private void GenerateStartingPlatform()
-    {
-        var playerPosition = _player.GlobalPosition;
-
-        var startX = (int)playerPosition.X / CellSize + _leftBoundary;
-        var startY = (int)playerPosition.Y / CellSize + 1;
-
-        for (var x = startX; x < startX + (_rightBoundary - _leftBoundary); x++)
-        {
-            for (var y = 0; y < StartingPlatformHeight; y++)
-            {
-                var tileCoord = (y == 0) ? _groundTileAtlasCoord : _groundTileAtlasCoord3;
-                _wallTileMap.SetCell(WallLayer, new Vector2I(x, startY + y), 0, tileCoord, 0);
-            }
-        }
-    }    private Vector2I GetRandomAtlasCoords()
+    private Vector2I GetRandomAtlasCoords()
     {
         var randomIndex = _rng.RandiRange(0, _backgroundTileCoords.Count - 1);
         return _backgroundTileCoords[randomIndex];
+    }
+
+
+    private int CalculateTilesFromPixels(int pixels)
+    {
+        return pixels / CellSize;
     }
 }
